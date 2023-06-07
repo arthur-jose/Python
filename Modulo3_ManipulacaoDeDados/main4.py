4bf5d9509fad3e1a41773ed65833c8849a84c95c
arthur-jose <lusfarra123@gmail.com>
2023-06-07 16:59:31 -0300

    commit

---
+++ Modulo3_ManipulacaoDeDados/main4.py
@@ -0,0 +1,10 @@
+
+import pandas as pd
+import numpy as np
+from pandas_datareader import data
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+azul_df = data.DataReader(name='AZUL4.SA', data_source='yahoo', start='2017-01-01')
+print(azul_df)
